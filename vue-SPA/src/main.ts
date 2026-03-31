4172c878fa5f077a2fe69601b1c8db560c2638ef
Githmi Jayakody <githmijayakody@gmail.com>
2026-04-01 00:28:46 +0530

    Install Pinia and Initialize in main.ts

--- vue-SPA/src/main.ts
+++ vue-SPA/src/main.ts
@@ -3,6 +3,11 @@ import './style.css'
 import router from './router/index'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
+
